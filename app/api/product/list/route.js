4354e82bb1d517e1411a4ede4e4527eccd960e66
RayArena <chandreshsingh2902@gmail.com>
2025-03-21 19:26:39 +0530

    Add Order place function

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,14 +1,23 @@
 import connectDBm from '@/config/dbm'
 import Product from '@/models/Product'
 import { NextResponse } from 'next/server'
+import { getAuth } from '@clerk/nextjs/server'
+import Address from '@/models/Address'
+import Order from '@/models/Order'
 
 export async function GET(request) {
   try {
 
+    const { userId } = getAuth(request)
+
     await connectDBm()
 
-    const products = await Product.find({})
-    return NextResponse.json({ success:true, products })
+    Address.length
+    Product.length
+
+    const orders = await Order.find({userId}).populate('address items.product')
+
+    return NextResponse.json({ success:true, orders })
 
   } catch (error) {
     return NextResponse.json({ success:false, message: error.message })
